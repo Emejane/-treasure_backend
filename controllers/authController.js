0f0092c724fb193a1d63a953086d59dfc9b75175
mahasegni.inbox@gmail.com <MahaSegni>
2024-06-21 22:54:04 +0200

    new function; Get user info + update profile + forget password with mail to reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const validator = require('validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const nodemailer = require('nodemailer');
 exports.register = [
     
     check('username', 'Username is required').notEmpty().trim().escape(),
@@ -99,3 +99,114 @@ exports.signOut = async (req, res) => {
         res.status(500).send('Server error');
     }
 };
+
+exports.getInfoUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.updateProfile = [
+    check('username', 'Username is required').optional().notEmpty().trim().escape(),
+    check('email', 'Please include a valid email').optional().isEmail().normalizeEmail(),
+    check('password', 'Password must be at least 6 characters').optional().isLength({ min: 6 }).trim().escape(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { username, email, password } = req.body;
+
+        try {
+            const user = await User.findById(req.user.id);
+            if (!user) {
+                return res.status(404).json({ msg: 'User not found' });
+            }
+
+            if (username) user.username = username;
+            if (email) user.email = email;
+            if (password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+            }
+
+            await user.save();
+            res.json(user);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+];
+
+exports.forgotPassword = [
+    check('email', 'Please include a valid email').isEmail().normalizeEmail(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email } = req.body;
+
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(404).json({ msg: 'User not found' });
+            }
+            const newPassword = generateRandomPassword();
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+            user.password = hashedPassword;
+            await user.save();
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            });
+
+            const mailOptions = {
+                from: process.env.EMAIL_USER,
+                to: user.email,
+                subject: 'Password Recovery for Treasure Caching',
+                html: `
+                    <p>Hello ${user.username},</p>
+                    <p>We received a request to reset your password for your account in the game <strong>Treasure Caching</strong>.</p>
+                    <p>Your new temporary password is: <strong>${newPassword}</strong></p>
+                    <p>Please use this temporary password to log in to your account. After logging in, we recommend you change your password to something more secure.</p>
+                    <p>If you did not request this password reset, please ignore this email and your password will remain unchanged.</p>
+                    <p>Thank you,</p>
+                    <p>The Treasure Caching Team</p>
+                `
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return res.status(500).json({ msg: 'Error sending email' });
+                }
+                res.status(200).json({ msg: 'Email sent' });
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+];
+
+function generateRandomPassword() {
+    return Math.random().toString(36).slice(-8);
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, signOut } = require('../controllers/authController');
+const { register, login, signOut, getInfoUser, updateProfile, forgotPassword} = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
 /**
  * @swagger
@@ -103,5 +104,87 @@ router.post('/login', login);
  *         description: Server error
  */
 router.post('/logout/:id', signOut);
+
+/**
+ * @swagger
+ * /auth/profile:
+ *   get:
+ *     summary: Get user information
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User information retrieved successfully
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
+router.get('/profile', auth, getInfoUser);
+
+/**
+ * @swagger
+ * /auth/updateProfile:
+ *   put:
+ *     summary: Update user profile
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's username
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *               password:
+ *                 type: string
+ *                 description: The user's password
+ *     responses:
+ *       200:
+ *         description: User profile updated successfully
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
+router.put('/updateProfile', auth, updateProfile);
+
+/**
+ * @swagger
+ * /auth/forgotPassword:
+ *   post:
+ *     summary: Send password recovery email
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *     responses:
+ *       200:
+ *         description: Email sent successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Server error
+ */
+router.post('/forgotPassword', forgotPassword);
 
 module.exports = router;
